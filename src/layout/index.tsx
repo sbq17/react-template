fa19e7620fe9eaebb3b602900c20908784f4d6be
heiyu17 <2010426591@qq.com>
2024-09-05 16:55:57 +0800

    修改layout布局

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,8 +4,10 @@ import { CSSProperties, LegacyRef, Suspense } from 'react'
 import { routes as routesList } from '@/router'
 import { Route, Routes, useRoutes } from 'react-router-dom'
 
+import './index.css'
+
 export default forwardRef(function LayoutComponent(props, ref) {
-	const { Header, Footer, Content } = Layout
+	const { Header, Footer, Content, Sider } = Layout
 
 	const headerStyle: CSSProperties = {
 		height: 36,
@@ -42,24 +44,41 @@ export default forwardRef(function LayoutComponent(props, ref) {
 	const notLayoutRoutes = routesList.filter(({ useLayout }) => useLayout === false)
 	const isUseLayout = routes ? findIndex(notLayoutRoutes, { path: routes.props.match.pathname }) === -1 : true
 
+	/**
+	 * 主体区域内容
+	 */
+	const pageContentNode = (
+		<Content style={contentStyle} className="layout_content_css">
+			<Suspense>
+				<Routes>
+					{routesList.map((item) => (
+						<Route {...item} key={item.path}></Route>
+					))}
+				</Routes>
+			</Suspense>
+		</Content>
+	)
+
 	return (
-		<Layout {...props} style={layoutStyle} ref={ref as LegacyRef<HTMLDivElement>}>
+		<Layout {...props} style={layoutStyle} ref={ref as LegacyRef<HTMLDivElement>} rootClassName="layout_wrapper">
 			{isUseLayout ? (
-				<Header style={headerStyle}>
-					<LayoutHeader />
-				</Header>
-			) : null}
-			<Content style={contentStyle}>
-				<Suspense>
-					<Routes>
-						{routesList.map((item) => (
-							<Route {...item} key={item.path}></Route>
-						))}
-						<Route></Route>
-					</Routes>
-				</Suspense>
-			</Content>
-			{isUseLayout ? <Footer style={footerStyle}>Footer</Footer> : null}
+				<>
+					{/* 侧边栏 */}
+					<Sider width="25%">Sider</Sider>
+					<Layout>
+						{/* 顶部栏 */}
+						<Header style={headerStyle}>
+							<LayoutHeader />
+						</Header>
+						{/* 主体渲染区域 */}
+						{pageContentNode}
+						{/* 底部区域 */}
+						<Footer style={footerStyle}>Footer</Footer>
+					</Layout>
+				</>
+			) : (
+				pageContentNode
+			)}
 		</Layout>
 	)
 })
