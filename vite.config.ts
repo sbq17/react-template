9fd29f659b0e5e0dfd5c8945d13d7a0d8a122083
heiyu17 <2010426591@qq.com>
2026-01-05 14:07:08 +0800

    Revert "feat: 添加 Ant Design ConfigProvider 并设置组件大小为 small"
    This reverts commit f84bf0f1f45b87725a17cad51fd9fc3c2c12677c, reversing
    changes made to 058bff9fef99dc162413392a0294a87d2fbdacf8.

--- vite.config.ts
+++ vite.config.ts
@@ -5,30 +5,47 @@ import tailwindcss from '@tailwindcss/vite' // 导入 Tailwind Vite 插件
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		react({
-			tsDecorators: true
-		}),
-		tailwindcss({}),
-		AutoImport({
-			imports: [
-				'react',
-				{
-					from: 'react-router-dom',
-					imports: ['RouteProps', 'RouteObject'],
-					type: true
-				},
-				{
-					from: './src/types/route.d.ts',
-					imports: ['CustomRouteProp'],
-					type: true
-				}
-			],
-			dts: 'types/auto-imports.d.ts',
-			dirs: ['./src/components/**', './src/api/**', './src/hook/**'],
-			// resolvers: [AntdResolver()],
-			eslintrc: { enabled: true, filepath: './.eslintrc-auto-import.js' }
-		})
-	]
+  plugins: [
+    react({
+      tsDecorators: true,
+    }),
+    tailwindcss({}),
+    AutoImport({
+      imports: [
+        'react',
+        // 'ahooks',
+        // {
+        // 	'lodash-es': [
+        // 		'isEmpty',
+        // 		'find',
+        // 		'findIndex',
+        // 		'reduce',
+        // 		'sortBy',
+        // 		'cloneDeep',
+        // 		'round',
+        // 		'orderBy',
+        // 		'map',
+        // 		'forEach',
+        // 		'filter',
+        // 		'round',
+        // 		'debounce'
+        // 	]
+        // },
+        {
+          from: 'react-router-dom',
+          imports: ['RouteProps', 'RouteObject'],
+          type: true
+        },
+        {
+          from: './src/types/route.d.ts',
+          imports: ['CustomRouteProp'],
+          type: true
+        }
+      ],
+      dts: 'types/auto-imports.d.ts',
+      dirs: ['./src/components/**', './src/api/**', './src/hook/**'],
+      // resolvers: [AntdResolver()],
+      eslintrc: { enabled: true, filepath: './.eslintrc-auto-import.js' }
+    }),
+  ]
 })
-
