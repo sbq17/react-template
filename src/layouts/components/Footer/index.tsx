9fcd7457eb68381c8b8e1aed13b79175abb7dfe9
heiyu17 <2010426591@qq.com>
2026-01-05 16:01:20 +0800

    feat(layout): 添加底部页脚组件
    在布局中添加 Footer 组件，并为其添加样式，完善页面整体结构

---
+++ src/layouts/components/Footer/index.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+	return <div className="app_layout_footer">Footer</div>
+}
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,11 +1,13 @@
 import AppMain from './components/AppMain'
 import Toolbox from './components/Toolbox'
+import Footer from './components/Footer'
 import './index.css'
 export default function Layout() {
 	return (
 		<div className="app_layout_wrapper">
 			<Toolbox></Toolbox>
 			<AppMain></AppMain>
+			<Footer></Footer>
 		</div>
 	)
 }
